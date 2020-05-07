c6ae02de328c9ffcaa6da4c8c47c4bfcaeee6d56
Andrew <xzy565143480@163.com>
2020-05-07 18:02:09 +0800

    gitignore

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,11 +2,11 @@
 App({
   //最开始的时候请自行修改此处的用户名密码，用于登陆数据管理后台进行邮件收发
   globalData: {
-    username: 'sudaxghd@163.com',
-    password: 'huodongbu'
+    username: 'xxx@163.com',
+    password: 'xxx',
   },
 
-  onLaunch: function() {
+  onLaunch: function () {
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -16,8 +16,8 @@ App({
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
         env: 'kexiedrama-ktb07',
-        traceUser: true
+        traceUser: true,
       })
     }
-  }
+  },
 })
