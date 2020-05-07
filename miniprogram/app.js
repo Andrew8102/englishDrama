8e62bdd154ec087a34d1a0465790f85fa1cfd2f8
Andrew Xu <37863310+Andrew8102@users.noreply.github.com>
2020-05-07 18:13:50 +0800

    Update app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,11 +3,7 @@ App({
   //最开始的时候请自行修改此处的用户名密码，用于登陆数据管理后台进行邮件收发
   globalData: {
     username: 'xxx@163.com',
-<<<<<<< HEAD
     password: 'xxx'
-=======
-    password: 'xxx',
->>>>>>> c6ae02d... gitignore
   },
 
   onLaunch: function () {
@@ -19,13 +15,8 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-<<<<<<< HEAD
         env: 'xxxxxx',
         traceUser: true
-=======
-        env: 'kexiedrama-ktb07',
-        traceUser: true,
->>>>>>> c6ae02d... gitignore
       })
     }
   },
